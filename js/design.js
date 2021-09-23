9ec1468af0e04bf6b61bfbd4536c8e7ab45bdc34
Gardoo <35525658+Gardoo@users.noreply.github.com>
2021-09-24 02:48:41 +0800

    homepage

--- js/design.js
+++ js/design.js
@@ -1,90 +1,55 @@
-// get our elements
-const slider = document.querySelector('.slider-container'),
-  slides = Array.from(document.querySelectorAll('.slide'))
+{
 
-// set up our state
-let isDragging = false,
-  startPos = 0,
-  currentTranslate = 0,
-  prevTranslate = 0,
-  animationID,
-  currentIndex = 0
-
-// add our event listeners
-slides.forEach((slide, index) => {
-  const slideImage = slide.querySelector('img')
-  // disable default image drag
-  slideImage.addEventListener('dragstart', (e) => e.preventDefault())
-  // touch events
-  slide.addEventListener('touchstart', touchStart(index))
-  slide.addEventListener('touchend', touchEnd)
-  slide.addEventListener('touchmove', touchMove)
-  // mouse events
-  slide.addEventListener('mousedown', touchStart(index))
-  slide.addEventListener('mouseup', touchEnd)
-  slide.addEventListener('mousemove', touchMove)
-  slide.addEventListener('mouseleave', touchEnd)
-})
-
-// make responsive to viewport changes
-window.addEventListener('resize', setPositionByIndex)
-
-// prevent menu popup on long press
-window.oncontextmenu = function (event) {
-  event.preventDefault()
-  event.stopPropagation()
-  return false
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
 }
 
-function getPositionX(event) {
-  return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
 }
 
-// use a HOF so we have index in a closure
-function touchStart(index) {
-  return function (event) {
-    currentIndex = index
-    startPos = getPositionX(event)
-    isDragging = true
-    animationID = requestAnimationFrame(animation)
-    slider.classList.add('grabbing')
+/***********************Login Form*************************/
+
+function admin(){
+	 	document.getElementById("form").style.display = "flex";
+
+	 }
+	 function closeForm(){
+	 	document.getElementById("form").style.display = "none";
+
+	 }
+	 function openLogin(){
+	 	document.getElementById("form2").style.display = "flex";
+	 	document.getElementById("myNav").style.width = "0%";
+
+	 }
+	 function closeForm2(){
+	 	document.getElementById("form2").style.display = "none";
+
+}	
+
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
 }
 
-function touchMove(event) {
-  if (isDragging) {
-    const currentPosition = getPositionX(event)
-    currentTranslate = prevTranslate + currentPosition - startPos
-  }
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
-function touchEnd() {
-  cancelAnimationFrame(animationID)
-  isDragging = false
-  const movedBy = currentTranslate - prevTranslate
 
-  // if moved enough negative then snap to next slide if there is one
-  if (movedBy < -100 && currentIndex < slides.length - 1) currentIndex += 1
-
-  // if moved enough positive then snap to previous slide if there is one
-  if (movedBy > 100 && currentIndex > 0) currentIndex -= 1
-
-  setPositionByIndex()
-
-  slider.classList.remove('grabbing')
-}
-
-function animation() {
-  setSliderPosition()
-  if (isDragging) requestAnimationFrame(animation)
-}
-
-function setPositionByIndex() {
-  currentTranslate = currentIndex * -window.innerWidth
-  prevTranslate = currentTranslate
-  setSliderPosition()
-}
-
-function setSliderPosition() {
-  slider.style.transform = `translateX(${currentTranslate}px)`
-}
+ }
+ 
