e5fe77bf2108565c63ae4ff7501ee18984d02877
Tsololate <floresjake021600@gmail.com>
2021-12-05 14:40:22 +0800

    asd

--- Sample.php
+++ Sample.php
@@ -1,126 +1,309 @@
 <!DOCTYPE html>
+
 <html>
+
 <head>
-<style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-  table-layout: fixed;
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<meta name="viewport" id="viewport" content="target-densitydpi=high-dpi,initial-scale=1.0,user-scalable=no" />
+
+<title>Document Title</title>
+
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+
+ 
+
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+
+<script type="text/javascript">
+
+var slideIndex = 1;
+
+var t;
+
+var dots;
+
+var maxheight = 0;
+
+var timedelay = 5000;
+
+window.onload = function() {
+
+var x = document.getElementsByClassName("mySlides");
+
+var parent_elem = document.getElementById('myslideFrame')
+
+ 
+
+var widthRatio = parseInt(parent_elem.offsetWidth) / parseInt(x[0].width)
+
+for (i = 0; i < x.length; i++) {
+
+// x.style.width = "100%";
+
+ 
+
+if (x.height > maxheight)
+
+{
+
+ 
+
+maxheight = x.height;
+
+ 
+
 }
 
-td, th {
-  border: 1px solid black;
-  text-align: left;
-  padding: 8px;
-  word-wrap: break-word;
+ 
+
 }
 
-table tr button { 
-  opacity:0;
-  float: left;
+ 
+
+document.getElementById('myslideFrame').style.height = maxheight * widthRatio + "px";
+
+ 
+
+showDivs(slideIndex);
+
+carousel();
+
+};
+
+ 
+
+function plusDivs(n) {
+
+showDivs(slideIndex += n);
+
+ 
+
 }
 
-table tr:hover button {
-  opacity:1 
+ 
+
+function currentDiv(n) {
+
+showDivs(slideIndex = n);
+
 }
 
-button{
-  background-color: #ffff33;
-  color: black;
-  border-radius: 8px;
-  margin: 1px;
-  font-size: 12px;
-  border: 1px solid black;
-  padding: 10px 24px;
-  cursor: pointer;
-}
-.dabtn{
-  background-color: #ff6666;
+ 
+
+function showDivs(n) {
+
+var i;
+
+var x = document.getElementsByClassName("mySlides");
+
+dots = document.getElementsByClassName("demo");
+
+if (n > x.length) {
+
+slideIndex = 1;
+
 }
 
+if (n < 1) {
+
+slideIndex = x.length;
+
+}
+
+;
+
+ 
+
+for (i = 0; i < x.length; i++) {
+
+x.style.display = "none";
+
+}
+
+for (i = 0; i < dots.length; i++) {
+
+dots.className = dots.className.replace(" w3-border-red", "");
+
+}
+
+x[slideIndex - 1].style.display = "inline-block";
+
+dots[slideIndex - 1].className += " w3-border-red";
+
+}
+
+ 
+
+ 
+
+ 
+
+function carousel() {
+
+var i;
+
+ 
+
+var x = document.getElementsByClassName("mySlides");
+
+for (i = 0; i < x.length; i++) {
+
+x.style.display = "none";
+
+dots.className = dots.className.replace(" w3-border-red", "");
+
+}
+
+slideIndex++;
+
+if (slideIndex > x.length) {
+
+slideIndex = 1
+
+}
+
+x[slideIndex - 1].style.display = "inline-block";
+
+dots[slideIndex - 1].className += " w3-border-red";
+
+t = setTimeout(carousel, timedelay);
+
+}
+
+ 
+
+function pauseCarousel() {
+
+ 
+
+clearTimeout(t)
+
+}
+
+ 
+
+function startCarousel() {
+
+ 
+
+t = setTimeout(carousel, timedelay);
+
+}
+
+ 
+
+</script>
+
+<style type="text/css">
+
+.w3-content {max-width:500px; margin: 10px auto; }
+
+.mySlides {width: 100%;} /**/
+
+.mySlides {display: none; vertical-align: middle; }
+
+#myslideFrame{text-align: center; font-size:0;min-height:100%; font-size:0; text-align: center;position:relative;}
+
+#myslideFrame:before, .flexbox > div:before {content: ""; height: 100%; vertical-align: middle; width: 0; background: red; display: inline-block;}
+
+ 
+
+.flexbox {
+
+display: flex;
+
+flex-wrap: wrap;
+
+margin: 2% 0 !important;
+
+}
+
+.flexbox > div{
+
+flex: 1;
+
+vertical-align: middle;
+
+font-size:0;
+
+}
+
+ 
+
+.flexbox > div img {vertical-align: middle;}
+
+ 
+
+.w3-left, .w3-right, .w3-badge {cursor:pointer}
+
+.w3-badge {height:13px;width:13px;padding:0}
+
+ 
 
 </style>
+
 </head>
+
 <body>
 
-<h2>Manage Accounts</h2>
+<div class="w3-content">
 
-<table>
-  <tr>
-    <th style="width:100px">Username</th>
-    <th style="width:100px">Name</th>
-    <th style="width:80px">Status</th>
-    <th style="width:250px">Address</th>
-    <th style="width:150px">City</th>
-    <th style="width:150px">Branch</th>
-    <th style="width:250px">E-mail</th>
-    <th style="width:80px">Level</th>
-    <th style="width:250px; border: none;"></th>
-  </tr>
+<div id="myslideFrame">
 
-  <tr>
-    <td>tsokolate</td>
-    <td>flores jake</td>
-    <td>active</td>
-    <td>286 kabatuhan rd depro caloocan city</td>
-    <td>caloocan</td>
-    <td>caloocan</td>
-    <td>floresjake021600@gmil.com</td>
-    <td>customer</td>
-    <td style="border: none;"><button>Change Level</button>  <button class="dabtn">Deactivate</button></td>
-  </tr>
+<img class="mySlides" src="http://www.w3schools.com/w3css/img_nature_wide.jpg" >
 
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td style="border: none;"><button>Change Level</button>  <button class="dabtn">Deactivate</button></td>
-  </tr>
+<img class="mySlides" src="http://www.w3schools.com/w3css/img_fjords_wide.jpg" >
 
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td style="border: none;"><button>Change Level</button>  <button class="dabtn">Deactivate</button></td>
-  </tr>
+<img class="mySlides" src="http://www.w3schools.com/w3css/img_mountains_wide.jpg" >
 
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td style="border: none;"><button>Change Level</button>  <button class="dabtn">Deactivate</button></td>
-  </tr>
+<a class="w3-btn-floating w3-hover-dark-grey" style="position:absolute;top:45%;left:0" onclick="plusDivs(-1)" onmouseover="pauseCarousel();" onmouseout="startCarousel()">❮</a>
 
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td style="border: none;"><button>Change Level</button>  <button class="dabtn">Deactivate</button></td>
-  </tr>
-</table>
+<a class="w3-btn-floating w3-hover-dark-grey" style="position:absolute;top:45%;right:0" onclick="plusDivs(1)" onmouseover="pauseCarousel();" onmouseout="startCarousel()">❯</a>
 
+<div style="width:100%" class="w3-center w3-section w3-large w3-text-white w3-display-bottomleft">
+
+<span onclick="currentDiv(1)" class="w3-badge demo w3-border w3-transparent w3-hover-white" onmouseover="pauseCarousel();" onmouseout="startCarousel()"></span>
+
+<span onclick="currentDiv(2)" class="w3-badge demo w3-border w3-transparent w3-hover-white" onmouseover="pauseCarousel();" onmouseout="startCarousel()"></span>
+
+<span onclick="currentDiv(3)" class="w3-badge demo w3-border w3-transparent w3-hover-white" onmouseover="pauseCarousel();" onmouseout="startCarousel()"></span>
+
+</div>
+
+</div>
+
+<div class="w3-row-padding w3-section flexbox">
+
+<div class="w3-col s4">
+
+<img class="demo w3-border w3-hover-shadow" src="http://www.w3schools.com/w3css/img_nature_wide.jpg" style="width:100%" onclick="currentDiv(1)" onmouseover="pauseCarousel();" onmouseout="startCarousel()">
+
+</div>
+
+<div class="w3-col s4">
+
+<img class="demo w3-border w3-hover-shadow" src="http://www.w3schools.com/w3css/img_fjords_wide.jpg" style="width:100%" onclick="currentDiv(2)" onmouseover="pauseCarousel();" onmouseout="startCarousel()">
+
+</div>
+
+<div class="w3-col s4">
+
+<img class="demo w3-border w3-hover-shadow" src="http://www.w3schools.com/w3css/img_mountains_wide.jpg" style="width:100%" onclick="currentDiv(3)" onmouseover="pauseCarousel();" onmouseout="startCarousel()">
+
+</div>
+
+</div>
+
+ 
+
+</div>
+
+ 
 
 </body>
-</html>
 
+</html>
